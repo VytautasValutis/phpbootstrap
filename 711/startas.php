e21617fd838300b5c0c86d24c23dce877b8498f9
Vytautas Valutis <dnc908@gmail.com>
2023-03-10 19:55:23 +0200

    HW711 work

--- 711/startas.php
+++ 711/startas.php
@@ -1,3 +1,20 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $game = [ 
+        'zaid1' => $_POST['zaid1'],
+        'zaid2' => $_POST['zaid2'],
+        'taskai1' => 0,
+        'taskai2' => 0,
+        'rid1' => 0,
+        'rid2' => 0,
+    ];
+    $game = serialize($game);
+    file_put_contents(__DIR__ . '/game.ser', $game);
+
+    header('Location: http://localhost/phpbootstrap/711/zaidimas.php');
+    die;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,18 +29,20 @@
     <div class="d-flex justify-content-center mt-5">
         <h2> Zaidimo pradzia </h2>
     </div>
-    <div class="row justify-content-center mt-5">
-        <div class="col-4 float-end">
-            <label> Vardas 1 </label>
-            <input type="text" class="form-control" id="zaid1">
+    <form action="" method="post">
+        <div class="row justify-content-center mt-5">
+            <div class="col-4 float-end">
+                <label> Vardas 1 </label>
+                <input type="text" class="form-control" name="zaid1">
+            </div>
+            <div class="col-4">
+                <label> Vardas 2 </label>
+                <input type="text" class="form-control" name="zaid2">
+            </div>
         </div>
-        <div class="col-4">
-        <label> Vardas 2 </label>
-        <input type="text" class="form-control" id="zaid2">
-    </div>
-    </div>
-    <div class="d-flex justify-content-center mt-5">
-  <button type="submit" class="btn btn-success">pradeti</button>
-</div>
+        <div class="d-flex justify-content-center mt-5">
+            <button type="submit" class="btn btn-success">pradeti</button>
+        </div>
+    </form>
 </body>
 </html>
--- 711/zaidimas.php
+++ 711/zaidimas.php
@@ -1,3 +1,11 @@
+<?php
+    $game = unserialize(file_get_contents(__DIR__ . '/game.ser'));
+    if($game['rid2'] < $game['rid1']) {
+        $meta = $game['zaid1'];
+    } else {
+        $meta = $game['zaid1'];
+    };
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,23 +18,29 @@
 </head>
 <body>
     <div class="d-flex justify-content-center mt-5">
-        <h2> Vyksta zaidimas </h2>
+        <h2> Vyksta žaidimas </h2>
     </div>
     <div class="row justify-content-center mt-5">
-        <div class="col-4 float-end">
-            <label> Vardas 1 </label>
-            <div> 100 </div>
+        <div class="col-4">
+            <label class="col-4 float-end"> <?= $game['zaid1'] ?> </label>
         </div>
         <div class="col-4">
-        <label> Vardas 2 </label>
-        <div> 100 </div>
+            <label class="col-4"> <?= $game['zaid2'] ?> </label>
+        </div>
     </div>
+    <div class="row justify-content-center mt-5">
+        <div class="col-4">
+            <div class="float-end"> <?= $game['taskai1'] ?> </div>
+        </div>
+        <div class="col-4">
+            <div> <?= $game['taskai2'] ?> </div>
+        </div>
     </div>
     <div class="d-flex justify-content-center mt-5">
-        <h3> zaidejas Petras meta kauliuka </h3>
+        <h3> Žaidejas <span style="color: red"><?= $meta ?></span>  meta kauliuka </h3>
     </div>
     <div class="d-flex justify-content-center mt-5">
-  <button type="submit" class="btn btn-primary">ridenti kauliuka</button>
+  <button type="submit" class="btn btn-primary">Ridenti kauliuka</button>
 </div>
 </body>
 </html>
