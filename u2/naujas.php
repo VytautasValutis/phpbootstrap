e69a85058032b7e9fdab7d22cd086dda891564dd
Vytautas Valutis <dnc908@gmail.com>
2023-03-11 17:05:22 +0200

    naujas

--- u2/naujas.php
+++ u2/naujas.php
@@ -16,11 +16,7 @@
     <title>Sarasas</title>
 </head>
 <body>
-    <div class="d-flex justify-content-center mt-5">
-        <h2> 
-            <img style="width: 30px" src="../image/bank.svg"> Trijų kortų bankelis 
-        </h2>
-    </div>
+    <?php require __DIR__ . '/logo.php' ?>
         <div class="ml-4"style="margin-left: 100px">
         <a type="button" class="btn btn-outline-warning" href="http://localhost/phpbootstrap/u2/sarasas.php">Eiti i saskaitu sarasa</a>
         </div>
