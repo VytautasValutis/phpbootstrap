a11a2b774d57833768b03002e6cc8658504895ec
Vytautas Valutis <dnc908@gmail.com>
2023-03-11 12:11:31 +0200

    FIN

--- 711/pabaiga.php
+++ 711/pabaiga.php
@@ -1,3 +1,10 @@
+<?php
+    $game = unserialize(file_get_contents(__DIR__ . '/game.ser'));
+    $k = ((int)$game['taskai1'] <=> (int)$game['taskai2']);
+    $vardas = ($k == 1) ? $game['zaid1'] : $game['zaid2'];
+    $task_w = max($game['taskai1'], $game['taskai2']);
+    $task_l = min($game['taskai1'], $game['taskai2']);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,11 +20,17 @@
         <h2> Žaidimas baigtas: </h2>
     </div>
     <div class="d-flex justify-content-center mt-5">
-        <h3> Laimėjo Petras 100 : 25</h3>
-        <h3> Lygiosios </h3>
+        <?php if($k == 0) : ?>
+            <h3> Lygiosios </h3>
+        <?php else : ?>
+            <h3> Laimėjo <?= $vardas ?> <?=$task_w?> : <?= $task_l ?> </h3>
+        <?php endif ?>
     </div>
     <div class="d-flex justify-content-center mt-5">
-  <button type="submit" class="btn btn-secondary">Pradėti iš naujo</button>
+    <form action="http://localhost/phpbootstrap/711/startas.php" method="get">
+            <button type="submit" class="btn btn-secondary">Pradėti iš naujo</button>
+        </form>
+
 </div>
 </body>
 </html>
